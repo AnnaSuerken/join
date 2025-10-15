ffd70d6c29070414e6b3df2518f5181b1cdb0ae4
Justin Kamionka <justinkamionka@icloud.com>
2025-10-15 17:58:18 +0200

    edit register

--- js/signup.js
+++ js/signup.js
@@ -11,9 +11,11 @@ import {
   update as dbUpdate,
 } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-database.js";
 
-
 const form = document.getElementById("register-form");
 const statusBox = document.getElementById("status");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+const checkBox = document.getElementById("checkbox");
 
 function setStatus(msg, isError = false) {
   statusBox.style.display = "flex";
@@ -21,19 +23,28 @@ function setStatus(msg, isError = false) {
   statusBox.style.color = isError ? "crimson" : "inherit";
 }
 
-form?.addEventListener("submit", async (e) => {
+function checkForm() {
+  if ((password.value === password2.value) & checkBox.checked) {
+    return true;
+  } else {
+    setStatus(
+      "Passwörter stimmen nicht überein oder AGB nicht akzeptiert",
+      true
+    );
+    return false;
+  }
+}
+
+signupbutton?.addEventListener("click", async (e) => {
   e.preventDefault();
-  const displayName = e.target.displayName.value.trim();
-  const email = e.target.email.value.trim();
-  const password = e.target.password.value;
-
+  if (!checkForm()) return;
+  const displayName = document.getElementById("displayName").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value;
   setStatus("Registrierung läuft");
-
   try {
     const cred = await createUserWithEmailAndPassword(auth, email, password);
-
     await updateProfile(cred.user, { displayName });
-
     await set(ref(db, `users/${cred.user.uid}`), {
       uid: cred.user.uid,
       email,
@@ -43,9 +54,7 @@ form?.addEventListener("submit", async (e) => {
       provider: "password",
       isAnonymous: false,
     });
-
     setStatus("Registrierung erfolgreich.");
-    
     setTimeout(() => {
       window.location.href = "/login.html";
     }, 1500);
