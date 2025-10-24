0059a693784846a9d420b4a72988d3111fb8d90e
Justin Kamionka <justinkamionka@icloud.com>
2025-10-24 09:35:39 +0200

    Fix Scroll on Contacts

--- js/addtask.js
+++ js/addtask.js
@@ -1,8 +1,8 @@
 function createTask() {
-    const taskTitle = document.getElementById("task-title");
-    dbApi.pushData('/board/todo', {
-        taskTitle: taskTitle.value,
-        id: "",})
-        console.log(taskTitle.value);
-        
+  const taskTitle = document.getElementById("task-title");
+  dbApi.pushData("/board/todo", {
+    taskTitle: taskTitle.value,
+    id: "",
+  });
+  console.log(taskTitle.value);
 }
