fd457b2c1c23b3b777307e4bf3e3b55b87ebe3aa
Anna Sürken <a.suerken@hotmail.de>
2025-10-25 16:00:01 +0200

    updated add task button

--- js/addtask.js
+++ js/addtask.js
@@ -5,9 +5,19 @@
 
 async function createTask() {
   const taskTitle = document.getElementById("task-title");
+  const taskDescription = document.getElementById("task-description");
+  const taskDueDate = document.getElementById("task-due-date");
+  //const taskContacts = document.getElementById("task-contacts");
+  const taskCategory = document.getElementById("task-category");
+  const subtask = document.getElementById("subtask");
+
   const key = await dbApi.pushData("/board/todo", {
     headline: taskTitle.value,
     id: "",
+    description: taskDescription.value,
+    deadline: taskDueDate.value,
+    category: taskCategory.value,
+    subtask: subtask.value,
   });
   await dbApi.updateData(`board/todo/${key}`, { id: key });
   console.log(taskTitle.value);
