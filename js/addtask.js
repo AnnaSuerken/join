6de15f77af4775580f585ff72861b6db1349a94d
Anna Sürken <a.suerken@hotmail.de>
2025-10-25 17:34:58 +0200

    added function to change prio buttons upon click

--- js/addtask.js
+++ js/addtask.js
@@ -28,11 +28,25 @@ async function createTask() {
  * 
  */
 
-async function clearTask() {
+function clearTask() {
   document.getElementById("task-title").value = "";
   document.getElementById("task-description").value = "";
   document.getElementById("task-due-date").value = "";
   //const taskContacts = document.getElementById("task-contacts");
   document.getElementById("task-category").value = "Select task category";
   document.getElementById("subtask").value = "";
+}
+
+/**
+ * This function changes the appearance of priority buttons within add.task.section upon click
+ * 
+ */
+function setPriority(status){
+  let priorityStatus = document.getElementById(`prio-${status}`)
+  priorityStatus.innerHTML = "";
+  priorityStatus.innerHTML += priorityTemplate(status);
+}
+
+function priorityTemplate(status){
+  return `<div class="priority-btn-${status}" id="prio-${status}">${status}<img src="./assets/icons/${status}-white.svg" alt="${status}" class="${status}-img-active"></div>` 
 }
