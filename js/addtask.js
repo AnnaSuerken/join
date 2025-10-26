ea4b4c62ea48f51bb3a8e6ea5929d000a2515628
Anna Sürken <a.suerken@hotmail.de>
2025-10-26 16:39:57 +0100

    added prio reset to clear function

--- js/addtask.js
+++ js/addtask.js
@@ -30,21 +30,30 @@ async function createTask() {
  */
 
 function clearTask() {
+  const priorities = ["urgent", "medium", "low"];
   document.getElementById("task-title").value = "";
   document.getElementById("task-description").value = "";
   document.getElementById("task-due-date").value = "";
   //const taskContacts = document.getElementById("task-contacts");
   document.getElementById("task-category").value = "Select task category";
   document.getElementById("subtask").value = "";
+  priorities.forEach((prio) => {
+    document.getElementById(`prio-${prio}`).classList.remove("d_none");
+    document.getElementById(`prio-${prio}-active`).classList.add("d_none");
+  });
+  currentPriority = [];
 }
 
 /**
  * This function changes the appearance of priority buttons within add.task.section upon click
  *
  */
+
+let currentPriority = [];
+
 function setPriority(status) {
   const priorities = ["urgent", "medium", "low"];
-  let currentPriority = null;
+
 
   if (currentPriority === status) {
     priorities.forEach((prio) => {
@@ -65,5 +74,5 @@ function setPriority(status) {
   document.getElementById(`prio-${status}-active`).classList.remove("d_none");
 
   currentPriority = status;
-  console.log(currentPriority)
+
 }
